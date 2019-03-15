d48e6794dc19144205f4d3c27496a73920c2afc7
Martin Holst Swende <martin@swende.se>
2019-03-15 23:39:36 +0100

    updated

--- README.md
+++ README.md
@@ -1,13 +1,15 @@
 This repo is a collection of vm statistics, 
 gathered from a geth instance. 
 
-Every data point represents `10k` blocks. 
-All opcodes are not represented in the charts.
+Every data point represents `10k` blocks. Every time an opcode executed, a timer was stopped, the time since start was noted, and
+the new opcode timer was started. After `10K` blocks, the data was dumped into a `json` file. The files are available in the `/m5.2xlarge/`-folder. 
 
-For time spent, most  opcodes are present. 
-For `Cost of ops`, only those with a static cost are included.
+The benchmarks are from a `m5.2xlarge` aws instance that did a full-sync. 
+Such a machine has 
 
-Examples from a `m5.2xlargge` aws instance that did a full-sync. 
+- `8 vcpu`, 
+- `32 Gb RAM`
+- `300 GB NVMe SSD`. 
 
 ### Time spent
 
@@ -15,23 +17,162 @@ Examples from a `m5.2xlargge` aws instance that did a full-sync.
 
 The large thing there is `SLOAD`. 
 
+If we place a cap on it, and filter so we only see the top four things that take up the execution  time, we can see that the top ones are
+
+- `SLOAD`, obviously, 
+- `SSTORE` - hardly surprising. Even though `SSTORE` is expensive, it's also one that touches disk.
+- `EXP` 
+- `JUMPI` - This is a _very_ common opcode. Also, Geth nowadays defer the jump analysis until it's actually needed - the first time a `JUMP`/`JUMPI` is called. 
+
+![](charts/timespentCapped.png)
+
+It seems that aside from `SLOAD`, there's no other single operation that dominates the remaining time that 
+is spent on block processing. 
+
 
 ## Cost of ops
 
-Are operations well-balanced, gas-wise. 
-Let's see
+Are operations well-balanced, gas-wise?
 
-![1](charts/timepergas0.png)
+### Arithmetic ops (`0x00`-range)
 
-![2](charts/timepergas5.png)
+![1](charts/arithmetics.png)
 
-![3](charts/timepergas10.png)
+The arithmetic ops seems to vary within one order of magnitude -- some peaks which is likely due to noisy data. 
+Here's a graph where it's been capped at `250`
 
-![4](charts/timepergas15.png)
-
-![5](charts/timepergas20.png)
+![1](charts/arithmetics_cap.png)
 
 
-And here's `SLOAD`
+### Comparison ops (`0x10`-range)
+
+![1](charts/comparison_cap.png)
+
+The comparisons ops (also capped at `250`) are fairly aligned within `35` and `130`. The `NOT` looks pretty over-represented. 
+
+### SHA3 ops (`0x20`-range)
+
+The `SHA3` operation has it's own range. Since it'a dynamically priced operation, the gas has not been part of the data collection.
+What we can do, however, is plot the time spent during `SHA3`, and the number of invocations.
+
+![1](charts/sha3.png)
+
+The two line up pretty well, so the op doesn't 'degrade' over time. As expected.
+
+## Context ops (`0x30`-range)
+
+The following ops have dynamic gas, and are not charted:
+
+	CALLDATALOAD,
+	CALLDATACOPY,
+	CODECOPY,
+	EXTCODECOPY,
+	RETURNDATACOPY,
+
+Here are the remainders (split up into two charts, both capped at `500`): 
+![2](charts/context1.png)
+The big spike around `2.08M` is, surprisingly, `CALLER`. Aside from a few spikes, they're fairly well aligned -- except for `BALANCE`, which is starting to 
+fluctuate and reaching high peaks. It was repriced in `EIP150`, anad was low for a few million blocks. 
+
+Note: `BALANCE` should really be called `EXTBALANCE`, since it fetches balance for (potentially) external accounts. 
+
+![2](charts/context2.png)
+
+The `EXTCODESIZE` looks _extremely_ cheap -- which makes sense, since it was repriced at 
+block `2463000` from `20` to `700`.
+
+The Tangerine Whistle HF also contained these changes:
+
+- Increase the gas cost of `EXTCODESIZE` to `700` (from `20`).
+- Increase the base gas cost of `EXTCODECOPY` to `700` (from `20`).
+- Increase the gas cost of `BALANCE` to `400` (from `20`).
+- Increase the gas cost of `SLOAD` to `200` (from `50`).
+- Increase the gas cost of `CALL`, `DELEGATECALL`, `CALLCODE` to `700` (from `40`).
+- Increase the gas cost of `SELFDESTRUCT` to 5000 (from `0`).
+
+##  Block operations (`0x40` range)
+
+This chart capped at `600`. 
+
+![2](charts/blockops_cap.png)
+
+Missing from this chart is `BLOCKHASH`, which deserves it's own chart. It's plotted along with it's opcode count -- 
+this operation is a bit quirky, and is more expensive when it's executed in isolation. So the more this op is used, the
+cheaper the operations become (to a degree). 
+This chart capped at `3000`. 
+
+![2](charts/blockhash.png)
+
+There are some further optimizations that can be done on this op, at least for geth. 
+
+## Storage and execution (`0x50` range)
+
+These are the ops
+
+	POP
+	MLOAD
+	MSTORE
+	MSTORE8
+	SLOAD
+	SSTORE
+	JUMP
+	JUMPI
+	PC
+	MSIZE
+	GAS
+	JUMPDEST
+
+However, 
+
+-`MSTORE` and `MSTORE8` have an additional cost for expanding memory
+- `SLOAD` varies depending on previous value
+- `JUMP`/`JUMPI` have a hidden cost: they require jumpdest analysis.
+
+So the first graph here shows `POP`, `MLOAD`, `SLOAD`,`PC`, `MSIZE` and `GAS`, 
+	
+![2](charts/storage1.png)
+
+It's clear that `SLOAD` went down at `EIP150`, but has started to rise significantly since then. At around `5M`, it was back 
+to the same levels as before `EIP150`. 
+
+## `0x60` range
+
+These are `DUPX`,`SWAPX` and `PUSHX`. Here capped for spikes, at `600`:
+
+![](charts/range60.png)
+
+Here capped at `100`:
+
+![](charts/range60p2.png)
+
+## `0xa0` - Logging
+
+The `LOG` opcodes are dynamically priced, depending on the memory size, so we don't have gas/time charts, but here's a 
+time spent-chart:
+
+![5](charts/logging.png)
+
+## `0xf0` - calling
+
+Remaining ops are 'special' - both dynamic costs and non-trivial effects, such as starting new call contexts
+or exiting from call contexts. 
+
+	CREATE
+	CALL
+	CALLCODE
+	RETURN
+	DELEGATECALL
+	CREATE2
+	STATICCALL
+	REVERT
+	SELFDESTRUCT
+
+# Some individual OPS
+
+## SLOAD
 
 ![5](charts/sload.png)
+
+## BALANCE
+
+![5](charts/balance.png)
--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 	"github.com/ethereum/go-ethereum/core/vm"
 	"github.com/ethereum/go-ethereum/params"
 	"github.com/wcharczuk/go-chart"
+	"github.com/wcharczuk/go-chart/drawing"
 	"io/ioutil"
 	"math/big"
 	"os"
@@ -108,14 +109,6 @@ func (dp *dataPoint) totalGas() uint64 {
 	return dp.count * dp.gas()
 }
 
-func (dp *dataPoint) mGasPerSec() float64 {
-	// gas / nanos * 1 000 M = gas / s
-	// (gas / 1000 000 ) / s = Mgas / s
-	// (gas / 1M ) * 1000M / nanos = Mgas / s
-	// (gas * 1000 ) / nanos = Mgas/s
-	return float64(1000*dp.totalGas()) / float64(dp.execTime)
-}
-
 func (dp *dataPoint) MilliSecondsPerMgas() float64 {
 	// gas / nanos * 1 000 M = gas / s
 	// (gas / 1000 000 ) / s = Mgas / s
@@ -125,31 +118,6 @@ func (dp *dataPoint) MilliSecondsPerMgas() float64 {
 		return float64(0)
 	}
 	return float64(1000*dp.execTime) / float64(1000*dp.totalGas())
-}
-
-func (dp *dataPoint) String() string {
-
-	return fmt.Sprintf("number %s, op %s; gas %d; count %d; execTime %d;execTime  %s; totalGas %d; MgasPerS %.03f",
-		dp.blockNumber,
-		dp.op.String(),
-		dp.gas(),
-		dp.count,
-		dp.execTime,
-		dp.execTime,
-		dp.totalGas(),
-		dp.mGasPerSec())
-}
-func (dp *dataPoint) CSV() string {
-
-	return fmt.Sprintf("number;%s;opcode;%s;gas;%d;count;%d;time;%d;time;%s;totalgas;%d;mgaspers;%.03f",
-		dp.blockNumber,
-		dp.op.String(),
-		dp.gas(),
-		dp.count,
-		dp.execTime,
-		dp.execTime,
-		dp.totalGas(),
-		dp.mGasPerSec())
 }
 
 func (dp *dataPoint) Sub(prev *dataPoint) *dataPoint {
@@ -196,8 +164,7 @@ func (stats *statCollection) collect(blnum int, data []byte) error {
 	return nil
 }
 
-
-func (stats *statCollection) series(op vm.OpCode, yFunc func(point *dataPoint) float64) ([]float64, []float64) {
+func (stats *statCollection) series(op vm.OpCode, fromBlock int, yFunc func(point *dataPoint) float64) ([]float64, []float64) {
 
 	var (
 		xseries []float64
@@ -211,13 +178,16 @@ func (stats *statCollection) series(op vm.OpCode, yFunc func(point *dataPoint) f
 
 	var prevBlock map[vm.OpCode]*dataPoint
 	for _, number := range numbers {
+		if number < fromBlock {
+			continue
+		}
 		block := stats.data[number]
 		if prevBlock != nil {
 			dp := block[op]
 			prevDp := prevBlock[op]
 			modDp := dp.Sub(prevDp)
 			// Only count it if it's been done more than 1000 times
-			if modDp.count > 1000{
+			if modDp.count > 500 {
 				yseries = append(yseries, yFunc(modDp))
 				xseries = append(xseries, float64(number))
 
@@ -228,21 +198,74 @@ func (stats *statCollection) series(op vm.OpCode, yFunc func(point *dataPoint) f
 	return xseries, yseries
 }
 
+// Construct a filter, which returns true if the any value in the given series is above the threshold
+func minFilter(threshold float64) func([]float64) bool {
+
+	return func(vals []float64) bool {
+		for _, v := range vals {
+			if v >= threshold {
+				return true
+			}
+		}
+		return false
+	}
+}
+
+type filterFn func(vals []float64) bool
+
 func plot(ops []vm.OpCode, stat statCollection, yFunc func(dp *dataPoint) float64, title, x, y, filename string) error {
+	return plotFilter(ops, stat, yFunc, title, x, y, filename, nil, 0)
+}
+func plotFilter(ops []vm.OpCode, stat statCollection, yFunc func(dp *dataPoint) float64, title, x, y, filename string, filter filterFn, fromBlock int) error {
+	showCount := len(ops) == 1
+	annotations := chart.AnnotationSeries{
+		Annotations: []chart.Value2{
+			{XValue: 2463000.0, YValue: 0, Label: "EIP150"},
+		}}
+
 	var series []chart.Series
 	for _, op := range ops {
-		xvals, yvals := stat.series(op, yFunc)
-		serie := chart.ContinuousSeries{
-			XValues: xvals,
-			YValues: yvals,
-			Name:    op.String(),
+		xvals, yvals := stat.series(op, fromBlock, yFunc)
+
+		if filter == nil || filter(yvals) {
+			serie := chart.ContinuousSeries{
+				XValues: xvals,
+				YValues: yvals,
+				Name:    op.String(),
+			}
+			series = append(series, serie)
+			if showCount {
+				// Show simple moving average
+				smaSerie := chart.SMASeries{
+					InnerSeries: serie,
+					Style: chart.Style{
+						Show:        true,
+						StrokeColor: drawing.ColorBlack,
+					},
+					Name: fmt.Sprintf("Moving AVG %v", serie.Name),
+				}
+				series = append(series, smaSerie)
+			}
+			if showCount {
+				secondaryYSeries, yvals := stat.series(op, fromBlock, func(dp *dataPoint) float64 {
+					return float64(dp.count)
+				})
+				countSerie := chart.ContinuousSeries{
+					XValues: secondaryYSeries,
+					YValues: yvals,
+					YAxis:   chart.YAxisSecondary,
+					Style: chart.Style{
+						StrokeColor: drawing.ColorRed,
+						Show:        true,
+					},
+					Name: "Count",
+				}
+				series = append(series, countSerie)
+			}
 		}
-		smaSerie := chart.SMASeries{
-			InnerSeries:serie,
-			Name: serie.Name,
-		}
-		series = append(series, smaSerie)
+
 	}
+	series = append(series, annotations)
 
 	graph := chart.Chart{
 		Title:      fmt.Sprintf(title),
@@ -258,8 +281,17 @@ func plot(ops []vm.OpCode, stat statCollection, yFunc func(dp *dataPoint) float6
 			NameStyle: chart.StyleShow(),
 			Style:     chart.StyleShow(),
 		},
+
 		Series: series,
 	}
+	if showCount {
+		graph.YAxisSecondary = chart.YAxis{
+			Name:      "Count",
+			NameStyle: chart.StyleShow(),
+			Style:     chart.StyleShow(), //enables / displays the secondary y-axis
+		}
+	}
+
 	graph.Elements = []chart.Renderable{
 		chart.LegendLeft(&graph),
 	}
@@ -273,36 +305,159 @@ func plot(ops []vm.OpCode, stat statCollection, yFunc func(dp *dataPoint) float6
 	return nil
 }
 
-var mostOps = []vm.OpCode{
-	vm.STOP, vm.ADD, vm.SUB, vm.LT, vm.GT, vm.SLT, vm.SGT,
-	vm.EQ, vm.ISZERO, vm.AND, vm.OR, vm.XOR, vm.NOT, vm.BYTE, vm.CALLDATALOAD,
+var RANGE0 = []vm.OpCode{
+	vm.ADD,
 	vm.MUL,
+	vm.SUB,
 	vm.DIV,
-	vm.SDIV, vm.MOD, vm.SMOD, vm.SIGNEXTEND,
+	vm.SDIV,
+	vm.MOD,
+	vm.SMOD,
 	vm.ADDMOD,
-	vm.MULMOD, vm.JUMP, vm.ADDRESS, vm.ORIGIN, vm.CALLER, vm.CALLVALUE,
-	vm.CALLDATASIZE, vm.CODESIZE, vm.GASPRICE, vm.COINBASE, vm.TIMESTAMP,
-	vm.NUMBER, vm.DIFFICULTY, vm.GASLIMIT, vm.POP, vm.PC, vm.MSIZE, vm.GAS,
-	vm.BLOCKHASH, vm.JUMPI, vm.JUMPDEST, vm.SLOAD, vm.EXTCODESIZE, vm.EXTCODECOPY,
-	vm.BALANCE, vm.EXTCODEHASH, vm.SHL,vm.SSTORE,
-	vm.SHR, vm.SAR, vm.CALL,
+	vm.MULMOD,
+	vm.EXP,
+	vm.SIGNEXTEND,
 }
-
-var someOps = []vm.OpCode{
-	vm.PUSH1,vm.PUSH2,
-	vm.PUSH32,
-	vm.ADDMOD,
-	vm.ADDRESS, vm.ORIGIN,
+var RANGE1 = []vm.OpCode{
+	vm.LT,
+	vm.GT,
+	vm.SLT,
+	vm.SGT,
+	vm.EQ,
+	vm.ISZERO,
+	vm.AND,
+	vm.OR,
+	vm.XOR,
+	vm.NOT,
+	vm.BYTE,
+	//vm.SHL,
+	//vm.SHR,
+	//vm.SAR,
+}
+var RANGE2 = []vm.OpCode{
+	vm.SHA3,
+}
+var RANGE3p1 = []vm.OpCode{
+	vm.ADDRESS,
+	vm.BALANCE,
+	vm.ORIGIN,
 	vm.CALLER,
 	vm.CALLVALUE,
-	vm.CALLDATASIZE, vm.CODESIZE, vm.GASPRICE, vm.COINBASE, vm.TIMESTAMP,
-	vm.NUMBER, vm.DIFFICULTY, vm.GASLIMIT, vm.POP, vm.PC, vm.MSIZE, vm.GAS,
-	vm.BLOCKHASH,
-	vm.JUMPI, vm.JUMPDEST,
-	vm.SLOAD,
+	vm.CALLDATASIZE,
+}
+
+var RANGE3p2 = []vm.OpCode{
+	vm.CODESIZE,
+	vm.GASPRICE,
 	vm.EXTCODESIZE,
-	vm.BALANCE, vm.EXTCODEHASH, vm.SHL,
-	vm.SHR, vm.SAR,
+	vm.RETURNDATASIZE,
+	vm.EXTCODEHASH,
+	//vm.CALLDATALOAD,
+	//vm.CALLDATACOPY,
+	//vm.CODECOPY,
+	//vm.EXTCODECOPY,
+	//vm.RETURNDATACOPY,
+}
+var RANGE4 = []vm.OpCode{
+	//vm.BLOCKHASH,
+	vm.COINBASE,
+	vm.TIMESTAMP,
+	vm.NUMBER,
+	vm.DIFFICULTY,
+	vm.GASLIMIT,
+}
+var RANGE4p2 = []vm.OpCode{
+	vm.BLOCKHASH,
+}
+var RANGE5p1 = []vm.OpCode{
+	vm.POP,
+	vm.MLOAD,
+	vm.SLOAD,
+	vm.PC,
+	vm.MSIZE,
+	vm.GAS,
+}
+var RANGE6 = []vm.OpCode{
+	vm.PUSH1,
+	vm.PUSH2,
+	vm.PUSH3,
+	vm.PUSH4,
+	vm.PUSH5,
+	vm.PUSH6,
+	vm.PUSH7,
+	vm.PUSH8,
+	vm.PUSH9,
+	vm.PUSH10,
+	vm.PUSH11,
+	vm.PUSH12,
+	vm.PUSH13,
+	vm.PUSH14,
+	vm.PUSH15,
+	vm.PUSH16,
+	vm.PUSH17,
+	vm.PUSH18,
+	vm.PUSH19,
+	vm.PUSH20,
+	vm.PUSH21,
+	vm.PUSH22,
+	vm.PUSH23,
+	vm.PUSH24,
+	vm.PUSH25,
+	vm.PUSH26,
+	vm.PUSH27,
+	vm.PUSH28,
+	vm.PUSH29,
+	vm.PUSH30,
+	vm.PUSH31,
+	vm.PUSH32,
+	vm.DUP1,
+	vm.DUP2,
+	vm.DUP3,
+	vm.DUP4,
+	vm.DUP5,
+	vm.DUP6,
+	vm.DUP7,
+	vm.DUP8,
+	vm.DUP9,
+	vm.DUP10,
+	vm.DUP11,
+	vm.DUP12,
+	vm.DUP13,
+	vm.DUP14,
+	vm.DUP15,
+	vm.DUP16,
+	vm.SWAP1,
+	vm.SWAP2,
+	vm.SWAP3,
+	vm.SWAP4,
+	vm.SWAP5,
+	vm.SWAP6,
+	vm.SWAP7,
+	vm.SWAP8,
+	vm.SWAP9,
+	vm.SWAP10,
+	vm.SWAP11,
+	vm.SWAP12,
+	vm.SWAP13,
+	vm.SWAP14,
+	vm.SWAP15,
+	vm.SWAP16,
+}
+
+var RANGE7 = []vm.OpCode{
+	vm.LOG0,
+	vm.LOG1,
+	vm.LOG2,
+	vm.LOG3,
+	vm.LOG4,
+}
+
+var allOps []vm.OpCode
+
+func init() {
+	for i := 0; i < 0xff; i++ {
+		allOps = append(allOps, vm.OpCode(i))
+	}
 }
 
 func main() {
@@ -331,9 +486,20 @@ func main() {
 	var time = func(dp *dataPoint) float64 {
 		return float64(dp.execTime) / 1000000
 	}
+	var timeCapped = func(dp *dataPoint) float64 {
+		v := float64(dp.execTime) / 1000000
+		if v < 100000 {
+			return v
+		}
+		return 100000
+	}
 
-	if err := plot(mostOps, stat, time, "Time spent", "Blocknumber", "Milliseconds",
-		"timespent.png"); err != nil {
+	if err := plot(allOps, stat, time, "Time spent", "Blocknumber", "Milliseconds", "timespent.png"); err != nil {
+		fmt.Printf("Error: %v", err)
+		syscall.Exit(1)
+	}
+	if err := plotFilter(allOps, stat, timeCapped, "Time spent", "Blocknumber", "Milliseconds",
+		"timespentCapped.png", minFilter(45000), 3220000); err != nil {
 		fmt.Printf("Error: %v", err)
 		syscall.Exit(1)
 	}
@@ -341,27 +507,105 @@ func main() {
 	var timepergas = func(dp *dataPoint) float64 {
 		return dp.MilliSecondsPerMgas()
 	}
-	var i = 0
-	for i := 0; i + 5 < len(someOps); i+= 5{
 
-		if err := plot(someOps[i:i+5], stat, timepergas,
-			"Millseconds per Mgas", "Blocknumber", "Milliseconds",
-			fmt.Sprintf("timepergas%d.png", i)); err != nil {
-			fmt.Printf("Error: %v", err)
-			syscall.Exit(1)
+	var timepergasCapAt = func(cap float64) func(*dataPoint) float64 {
+		return func(dp *dataPoint) float64 {
+			if g := dp.MilliSecondsPerMgas(); g < cap {
+				return g
+			}
+			return cap
 		}
-
 	}
-	if err := plot(someOps[i:i+5], stat, timepergas,
-		"Millseconds per Mgas", "Blocknumber", "Milliseconds",
-		fmt.Sprintf("timepergas%d.png", i)); err != nil {
+
+	if err := plot(RANGE0, stat, timepergas,
+		"Milliseconds per Mgas (0x00 opcodes - Arithmetic)", "Blocknumber", "Milliseconds",
+		fmt.Sprintf("arithmetics.png")); err != nil {
+		fmt.Printf("Error: %v", err)
+		syscall.Exit(1)
+	}
+
+	if err := plot(RANGE0, stat, timepergasCapAt(250.0),
+		"Milliseconds per Mgas (0x00 opcodes - Arithmetic) - capped", "Blocknumber", "Milliseconds",
+		fmt.Sprintf("arithmetics_cap.png")); err != nil {
+		fmt.Printf("Error: %v", err)
+		syscall.Exit(1)
+	}
+
+	if err := plot(RANGE1, stat, timepergasCapAt(250.0),
+		"Milliseconds per Mgas (0x10 opcodes - Comparison)", "Blocknumber", "Milliseconds",
+		fmt.Sprintf("comparison_cap.png")); err != nil {
+		fmt.Printf("Error: %v", err)
+		syscall.Exit(1)
+	}
+	if err := plot(RANGE2, stat, time,
+		"Time spent on (0x30 opcodes - SHA3)", "Blocknumber", "Milliseconds",
+		fmt.Sprintf("sha3.png")); err != nil {
+		fmt.Printf("Error: %v", err)
+		syscall.Exit(1)
+	}
+	if err := plot(RANGE3p1, stat, timepergasCapAt(500.0),
+		"Milliseconds per Mgas (0x30 opcodes - Context, part 1)", "Blocknumber", "Milliseconds",
+		fmt.Sprintf("context1.png")); err != nil {
+		fmt.Printf("Error: %v", err)
+		syscall.Exit(1)
+	}
+	if err := plot(RANGE3p2, stat, timepergasCapAt(500.0),
+		"Milliseconds per Mgas (0x30 opcodes - Context, part 2)", "Blocknumber", "Milliseconds",
+		fmt.Sprintf("context2.png")); err != nil {
+		fmt.Printf("Error: %v", err)
+		syscall.Exit(1)
+	}
+
+	if err := plot(RANGE4, stat, timepergasCapAt(600.0),
+		"Milliseconds per Mgas (0x40 opcodes - Block ops)", "Blocknumber", "Milliseconds",
+		fmt.Sprintf("blockops_cap.png")); err != nil {
+		fmt.Printf("Error: %v", err)
+		syscall.Exit(1)
+	}
+
+	if err := plot(RANGE4p2, stat, timepergasCapAt(3000.0),
+		"Milliseconds per Mgas (BLOCKHASH)", "Blocknumber", "Milliseconds",
+		fmt.Sprintf("blockhash.png")); err != nil {
+		fmt.Printf("Error: %v", err)
+		syscall.Exit(1)
+	}
+
+	if err := plot(RANGE5p1, stat, timepergasCapAt(3000.0),
+		"Milliseconds per Mgas (0x50 Storage and execution - part 1)", "Blocknumber", "Milliseconds",
+		fmt.Sprintf("storage1.png")); err != nil {
+		fmt.Printf("Error: %v", err)
+		syscall.Exit(1)
+	}
+	if err := plot(RANGE6, stat, timepergasCapAt(600.0),
+		"Milliseconds per Mgas (0x60 Pops, Swaps, Dups)", "Blocknumber", "Milliseconds",
+		fmt.Sprintf("range60.png")); err != nil {
+		fmt.Printf("Error: %v", err)
+		syscall.Exit(1)
+	}
+
+	if err := plot(RANGE6, stat, timepergasCapAt(100.0),
+		"Milliseconds per Mgas (0x60 Pops, Swaps, Dups) - capped at 100", "Blocknumber", "Milliseconds",
+		fmt.Sprintf("range60p2.png")); err != nil {
+		fmt.Printf("Error: %v", err)
+		syscall.Exit(1)
+	}
+
+	if err := plot(RANGE7, stat, time,
+		"Time spent on log operations (0x70 LOG) ", "Blocknumber", "Milliseconds",
+		fmt.Sprintf("logging.png")); err != nil {
 		fmt.Printf("Error: %v", err)
 		syscall.Exit(1)
 	}
 
 	if err := plot([]vm.OpCode{vm.SLOAD}, stat, timepergas,
-		"Millseconds per Mgas (SLOAD)", "Blocknumber", "Milliseconds",
+		"Milliseconds per Mgas (SLOAD)", "Blocknumber", "Milliseconds",
 		fmt.Sprintf("sload.png")); err != nil {
+		fmt.Printf("Error: %v", err)
+		syscall.Exit(1)
+	}
+	if err := plot([]vm.OpCode{vm.BALANCE}, stat, timepergas,
+		"Milliseconds per Mgas (BALANCE)", "Blocknumber", "Milliseconds",
+		fmt.Sprintf("balance.png")); err != nil {
 		fmt.Printf("Error: %v", err)
 		syscall.Exit(1)
 	}
